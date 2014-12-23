3caf8056bb2dfc2de8a0938bfa9b25138f92f32a
emile <emile.caron@outlook.com>
2014-12-23 23:29:34 +0100

    random image

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from utils import get_random_line
+
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def root():
     context = {
-        'image_url': 'https://media.joomeo.com/large/5499d3cdf34bd.jpg'
+        'image_url': get_random_line('urls.txt')
     }
     return render_template('index.html', **context)
 
---
+++ utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from random import choice
+
+def get_random_line(filename):
+    """
+    Return a random line from the file
+    """
+    
+    with open(filename, 'r') as fp:
+        return choice(fp.readlines())
