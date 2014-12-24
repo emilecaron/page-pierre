16b1d6aa8836cc8b288abe070597b4a0b3f5608f
emile <emile.caron@outlook.com>
2014-12-24 18:37:49 +0100

    go

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 
 from flask import Flask, render_template
 from utils import get_random_line
-
-
 app = Flask(__name__)
 
 @app.route("/")
--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@
 
 from random import choice
 
+
 def get_random_line(filename):
     """
     Return a random line from the file
     """
-    
-    with open(filename, 'r') as fp:
-        return choice(fp.readlines())
+
+    with open(filename, 'r') as f:
+        return choice(f.readlines())
+
