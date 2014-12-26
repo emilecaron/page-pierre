b88d921cddb7f6640fc72fa1981b8325645f5eeb
Emile CARON <ecaron@work4labs.com>
2014-12-26 18:52:02 +0100

    articles parser

--- utils.py
+++ utils.py
@@ -11,3 +11,50 @@ def get_random_line(filename):
     with open(filename, 'r') as f:
         return choice(f.readlines())
 
+class Parser(object):
+    """
+    Parse articles file
+    The file should look like that:
+        
+    ArticleName1
+    link1
+    link2
+    
+    ArticleName2
+    link1
+    link2
+
+    """
+
+    def parse_file(self, filename):
+        '''
+        Yield article_data dicts
+        '''
+        
+        with open(filename, 'r') as f:
+            article = None
+            urls = []
+
+            for line in f.readlines() + ['']:
+                line = line.strip()
+
+                if line:
+                    if not article:
+                        article = line
+                    else: 
+                        urls.append(line)
+                    continue
+                
+                if not article: 
+                    continue
+
+                yield dict(article=article, urls=urls)
+                
+                article = ''
+                urls = []
+
+
+if __name__ == '__main__':
+    for x in Parser().parse_file('articles.txt'):
+        print x
+
